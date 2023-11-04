70b09d6289dd29c6d1e3d192b129f2da3f0f767d
Adarsh Nandanwar <anandanwar@microsoft.com>
2023-11-05 00:25:54 +0530

    Update content 2023

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -47,32 +47,40 @@ $(document).ready(function () {
     // SKILLS
     var skillDetails = [
         { title: "Software Development", iconClass: "fas fa-laptop-code" },
+        { title: "Distributed Systems", iconClass: "fas fa-laptop-code" },
+        { title: "Cloud Computing", iconClass: "fas fa-laptop-code" },
+        { title: "System Design", iconClass: "fas fa-laptop-code" },
         { title: "Data Structures", iconClass: "fa fa-project-diagram" },
         { title: "Algorithms", iconClass: "fa fa-file-code" },
         { title: "Data Analysis", iconClass: "fas fa-chart-bar" },
+        { title: "Artificial intelligence", iconClass: "fas fa-robot" },
         { title: "Machine Learning", iconClass: "fas fa-robot" },
+        { title: "Deep Learning", iconClass: "fas fa-robot" },
+        { title: "Reinforcement Learning", iconClass: "fas fa-robot" },
+        { title: "Full Stack Web Development", iconClass: "fab fa-github" },
 
         { title: "C", iconClass: "" },
         { title: "C++", iconClass: "" },
+        { title: "C Sharp", iconClass: "" },
         { title: "Java", iconClass: "fab fa-java" },
         { title: "Python", iconClass: "fab fa-python" },
         { title: "Django", iconClass: "" },
         { title: "Flask", iconClass: "" },
-        { title: "Git", iconClass: "fab fa-git-alt" },
         { title: "HTML", iconClass: "fab fa-html5" },
         { title: "CSS", iconClass: "fab fa-css3-alt" },
         { title: "SQL", iconClass: "fas fa-database" },
+        { title: "Kusto Query Language", iconClass: "fas fa-database" },
         { title: "KDB+", iconClass: "fas fa-database" },
         { title: "Git & GitHub", iconClass: "fab fa-github" },
-        { title: "Microsoft Azure", iconClass: "fas fa-cloud" }
+        { title: "Microsoft Azure", iconClass: "fas fa-cloud" },
+        { title: "AWS", iconClass: "fas fa-cloud" }
     ]
     var oldSkillDetails = [
         { title: "JavaScript", iconClass: "fab fa-js" },
         { title: "jQuery", iconClass: "" },
         { title: "Node.js", iconClass: "fab fa-node-js" },
         { title: "MongoDB", iconClass: "fas fa-database" },
-        { title: "Heroku", iconClass: "fas fa-cloud" },
-        { title: "AWS", iconClass: "fas fa-cloud" }
+        { title: "Heroku", iconClass: "fas fa-cloud" }
     ]
     // adding skills in body
     var skillRow = document.createDocumentFragment();
@@ -126,6 +134,24 @@ $(document).ready(function () {
             link: ""
         },
         {
+            title: "Parser Translator for Modified 2048 Game Programming Language",
+            description: "A game programming language for a modified version of the famous 2048 game that allows other operations like subtraction, multiplication, and division in addition to the plain doubling operation during tile mergers. Specifically, created a scanner using Flex, a parser translator for a complete syntax‑directed translation scheme using Bison, a suitable grammar to represent the operations, as well as advanced error handling into the scheme (from semantic all the way down to lexical errors).",
+            image: "public/static/projects/2048.png",
+            link: "https://github.com/AdarshNandanwar/Modified-2048-Game-Programming-Language"
+        },
+        {
+            title: "Hybrid Wumpus Agent maze pathfinding using AI",
+            description: "Given a Wumpus world containing exactly one pit and one Wumpus agent in room [1,1]. The goal of the agent is to exit the Wumpus world alive. The agent can exit the Wumpus world by reaching room [4,4]. There will be a breeze in the rooms adjacent to the pit, and there will be a stench in the rooms adjacent to Wumpus. Wrote a python program that uses Propositional Logic sentences to check which rooms are safe. The inference is drawn using the Davis-Putnam-Logemann-Loveland (DPLL) algorithm. There will always be a safe path that the agent can take to exit the Wumpus world. The logical agent can take four actions: Up, Down, Left and Right. These actions help the agent move from one room to an adjacent room. The agent can perceive two things: Breeze and Stench.",
+            image: "public/static/projects/wumpus.png",
+            link: "https://github.com/AdarshNandanwar/hybrid-wumpus-agent-AI"
+        },
+        {
+            title: "Grammer Checker for English Language using NLP",
+            description: "Built a grammar checker from scratch using the concepts of Natural Language Processing. Given a corpus with some paragraphs labeled as correct and some as unknown, the aim was to find all the sentences that have errors. Correct paragraphs have no grammatical errors, while unknown may/may not have sentences which have grammatical errors in them. For example: “In the country there lived a fox. The quick brown fox jump over the fence. Farmer Shaun was terrified.” In this paragraph, the sentence “The quick brown fox jump over the fence” is grammatically incorrect.",
+            image: "public/static/projects/grammar.jpg",
+            link: "https://github.com/AdarshNandanwar/NLP-Grammar-Check-for-English-Language"
+        },
+        {
             title: "Image Classifier (OpenCV, Flask)",
             description: "Web aplication made using Flask and OpenCV. User may upload any image which is classified into predefined classes. These are then displayed in the main page with the appropriate tags.",
             image: "public/static/projects/imageClassifier.png",
@@ -143,7 +169,7 @@ $(document).ready(function () {
     var projectRow = document.createDocumentFragment();
     projectDetails.forEach(function (project, index) {
         var newProject = document.createElement('div');
-        newProject.className = 'col-md-6 col-lg-4';
+        newProject.className = 'col-md-6 col-lg-3';
         newProject.innerHTML =
             `<a data-toggle="modal" data-target="#myModal${index}">\
             <div class="card project-card">\
@@ -189,6 +215,18 @@ $(document).ready(function () {
     // RESEARCH
     var researchDetails = [
         {
+            title: "RL-MAGE: Strengthening Malware Detectors Against Smart Adversaries",
+            authors: "Adarsh Nandanwar, Hemant Rathore, Sanjay K. Sahay, Mohit Sewak",
+            description: "Today, android dominates the smartphone operating systems market. As per Google, there are over 3 billion active android users. With such a large population depending on the platform for their daily activities, a strong need exists to protect android from adversaries. Historically, techniques like signature and behavior were used in malware detectors. However, machine learning and deep learning models have now started becoming a core part of next-generation android malware detectors. In this paper, we step into malware developers/adversary shoes and ask: Are machine learning based android detectors resilient to reinforcement learning based adversarial attacks? Therefore, we propose the RL-MAGE framework to investigate the adversarial robustness of android malware detectors. The RL-MAGE framework assumes the grey-box scenario and aims to improve the adversarial robustness of malware detectors. We designed three reinforcement learning based evasion attacks A2C-MEA, TRPO-MEA, and PPO-MEA, against malware detectors. We investigated the robustness of 30 malware detection models based on 2 features (android permission and intent) and 15 distinct classifiers from 4 different families (machine learning classifiers, bagging based classifiers, boosting based classifiers, and deep learning classifiers). The designed evasion attacks generate adversarial applications by adding perturbations into the malware so that they force misclassifications and can evade malware detectors. The attack agent ensures that the adversarial applications’ structural, syntactical, and behavioral integrity is preserved, and the attack’s cost is minimized by adding minimum perturbations. The proposed TRPO-MEA evasion attack achieved a mean evasion rate of 93.27% while reducing the mean accuracy of 30 malware detectors from 85.81% to 50.29%. We also propose the ARShield defense strategy to improve the malware detectors’ classification performance and robustness. The TRPO-MEA ARShield models achieved 4.10% higher mean accuracy and reduced the mean evasion rate of re-attack from 93.27% to 1.05%. Finally, we conclude that the RL-MAGE framework improved the detection performance and adversarial robustness of malware detectors.",
+            link: "https://doi.org/10.1007/978-3-031-36021-3_6"
+        },        
+        {
+            title: "Adversarial superiority in android malware detection: Lessons from reinforcement learning based evasion attacks and defenses",
+            authors: "Hemant Rathore, Adarsh Nandanwar, Sanjay K. Sahay, Mohit Sewak",
+            description: "Today, android smartphones are being used by billions of users and thus have become a lucrative target of malware designers. Therefore being one step ahead in this zero-sum game of malware detection between the anti-malware community and malware developers is more of a necessity than a desire. This work focuses on a proactive adversary-aware framework to develop adversarially superior android malware detection models. We first investigate the adversarial robustness of thirty-six distinct malware detection models constructed using two static features (permission and intent) and eighteen classification algorithms. We designed two Targeted Type-II Evasion Attacks (TRPO-MalEAttack and PPO-MalEAttack) based on reinforcement learning to exploit vulnerabilities in the above malware detection models. The attacks aim to add minimum perturbations in each malware application and convert it into an adversarial application that can fool the malware detection models. The TRPO-MalEAttack achieves an average fooling rate of 95.75% (with 2.02 mean perturbations), reducing the average accuracy from 86.01% to 49.11% in thirty-six malware detection models. On the other hand, The PPO-MalEAttack achieves a higher average fooling rate of 96.87% (with 2.08 mean perturbations), reducing the average accuracy from 86.01% to 48.65% in the same thirty-six detection models. We also develop a list of the TEN most vulnerable android permissions and intents that an adversary can use to generate more adversarial applications. Later, we propose a defense strategy (MalVPatch) to counter the adversarial attacks on malware detection models. The MalVPatch defense achieves higher detection accuracy along with a drastic improvement in the adversarial robustness of malware detection models. Finally, we conclude that investigating the adversarial robustness of models is necessary before their real-world deployment and helps achieve adversarial superiority in android malware detection.",
+            link: "https://doi.org/10.1016/j.fsidi.2023.301511"
+        },
+        {
             title: "Deep Neural Networks on EEG Signals to Predict Auditory Attention Score Using Gramian Angular Difference Field",
             authors: "Mahak Kothari, Shreyansh Joshi, Adarsh Nandanwar, Aadetya Jaiswal, Veeky Baths",
             description: "Auditory attention is a selective type of hearing in which people focus their attention intentionally on a specific source of a sound or spoken words whilst ignoring or inhibiting other auditory stimuli. In some sense, the auditory attention score of an individual shows the focus the person can have in auditory tasks. The recent advancements in deep learning and in the non-invasive technologies recording neural activity beg the question, can deep learning along with technologies such as electroencephalography (EEG) be used to predict the auditory attention score of an individual? In this paper, we focus on this very problem of estimating a person's auditory attention level based on their brain's electrical activity captured using 14-channeled EEG signals. More specifically, we deal with attention estimation as a regression problem. The work has been performed on the publicly available Phyaat dataset. The concept of Gramian Angular Difference Field (GADF) has been used to convert time-series EEG data into an image having 14 channels, enabling us to train various deep learning models such as 2D CNN, 3D CNN, and convolutional autoencoders. Their performances have been compared amongst themselves as well as with the work done previously. Amongst the different models we tried, 2D CNN gave the best performance. It outperformed the existing methods by a decent margin of 0.22 mean absolute error (MAE).",
@@ -226,6 +264,7 @@ $(document).ready(function () {
                         <div class="row">\
                             <div class="col-md-12">\
                                 <h5 class="card-title">${project.title}</h5>\
+                                <h5 class="card-text">${project.authors}</h5>\
                             </div>\
                         </div>\
                         <div class="row modal-description">\
